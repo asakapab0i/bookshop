ff6789a506aeb6b617f9bdb1b20335236b1bcb56
ambowt <ambowt>
2014-01-23 00:16:16 +0800

    typo

--- application/views/search/search_view.php
+++ application/views/search/search_view.php
@@ -1,95 +1,238 @@
-{search_result}
 <div class="row">
-
-
 	
+   <?php
+   				
+   				$limit = array(
+   					10, 20, 50, 100
+   					);
+   				$order = array(
+   					'title' => 'Title',
+   					'price' => 'Price',
+   					'dateadd' => 'Date',
+   					);
+
+   				
+            	$link_segments =  array(
+              				 'page' => $this->uri->segment(8),
+              				 'limit' => $this->uri->segment(7),
+              				 'order' => $this->uri->segment(6), //asc, desc
+              				 'mode' => $this->uri->segment(4),
+              				 'order_by' => $this->uri->segment(5), 
+              				 'category' => $this->uri->segment(3)
+              				 );
+            	
+
+
+            	if ($link_segments['mode'] == '') {
+            		$link_segments['mode'] = 'grid';
+            	}
+            ?>
+
+
 
 
 <div class="col-md-9">
+	
+	<div class="panel panel-primary" id="panels">
+            <div class="panel-heading">Books | Category 
 
-<div class="panel panel-primary" id="panels">
-            <div class="panel-heading">Book Details</div>
-            
+            <select class="category">
+              <option value="all">All</option>
+			 <?php
+            	foreach ($category as $key => $value) {
+            		foreach ($value as $key2 => $value2) {
+            			
+            			if ($key2 == 'name') {
+
+            				
+            				if (strtolower($value[$key2]) == $link_segments['category']) {
+            					//echo $link_segments['category'];
+            					//echo $value[$key2];
+            					
+            					echo '<option selected value="'.strtolower($value[$key2]).'">'.$value[$key2].'</option>';
+            				}else{
+            					
+            					echo '<option value="'.strtolower($value[$key2]).'">'.$value[$key2].'</option>';;
+            				}
+
+            			}
+            		}
+            		
+            	}
+            ?>
+
+            				
+            		
+            	<!-- <option value="all">All Books</option>
+            	<option value="ecommerce">eCommerce Books</option>
+            	<option value="antiquesandcollectible">Antiques &amp; Collectibles</option>
+            	<option>16</option> -->
+            </select> 
+
+
+         
+
+
+
+            </div>
+         
+
             <div class="panel-body">
+            <center>
+
+            <?php
+
+            if ($this->session->flashdata('wishlist')) {
+              $notice = $this->session->flashdata('wishlist');
+
+              echo '<div class="alert alert-warning">';
+              echo $notice;
+              echo '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>';
+              echo '</div>';
+
+            }
+
+            ?>
+
+
+         {pagination}
+
+            </center>
+              <div class="col-md-12"> <small> View as: <a class="
+              <?php
+
+              if ($link_segments['mode'] == "grid") {
+              	echo 'btn btn-xs btn-default';
+              }
+
+              ?>
+
+              " href="<?php 
+
+              $default = 'grid';
+              $attrib = array('book/browse',$link_segments['category'], $default, $link_segments['order_by'], $link_segments['order'],$link_segments['limit'],$link_segments['page']);
+              echo site_url($attrib); ?>">Grid</a> <a class="
+
+
+              <?php
+
+              if ($link_segments['mode'] == "list") {
+              	echo 'btn btn-xs btn-default';
+              }
+
+              ?>
+
+              ?>
+
+
+              " href="
+
+             <?php 
+
+              $default = 'list';
+		      $attrib = array('book/browse',$link_segments['category'], $default, $link_segments['order_by'], $link_segments['order'],$link_segments['limit'],$link_segments['page']);
+              echo site_url($attrib); ?>
+
+              ">List</a> 
+             
+             	<div class="pull-right">
+             		Show per page: 
+			<select class="limit">
+
+				<?php
+					foreach ($limit as $key => $value) {
+
+
+						if ($value == $link_segments['limit']) {
+							echo '<option selected value='.$value.'>'.$value.'</option>';
+						}else{
+							echo '<option value='.$value.'>'.$value.'</option>';
+						}
+					}
+				?>
+            </select>
+            Order by: <select class="order_by">
+            	<?php
+
+            	foreach ($order as $key => $value) {
+            		
+            		if ($key == $link_segments['order_by']) {
+            			echo '<option selected value='.$key.'>'.$value.'</option>';
+            		}else{
+            			echo '<option value='.$key.'>'.$value.'</option>';
+            		}
+            	}
+
+            	?>
+            </select> 
+           
+
+
+
+            <?php
+            if ($link_segments['order'] == 'asc') {
+
+
+              $default = 'desc';
+              if ($link_segments['order_by'] == '') {
+              	$link_segments['order_by'] = 'title';
+              }
+
+              $attrib = array('book/browse',$link_segments['category'], $link_segments['mode'], $link_segments['order_by'], $default, $link_segments['limit'],$link_segments['page']);
+              $data = site_url($attrib); 
+
+            	echo '<a title="Descending" href="'.$data.'"><span class="glyphicon glyphicon-arrow-down"></span></a>';
+            }else{
+
+            	if ($link_segments['order_by'] == '') {
+              	$link_segments['order_by'] = 'title';
+              }
+
+			  $default = 'asc';
+              $attrib = array('book/browse',$link_segments['category'], $link_segments['mode'], $link_segments['order_by'], $default, $link_segments['limit'],$link_segments['page']);
+              $data = site_url($attrib); 
+
+            	echo '<a title="Ascending" href="'.$data.'"><span class="glyphicon glyphicon-arrow-up"></span></a>';
+            }
+            ?>
+
+             	</div>
+
+</small>
+              </div>
+
+
             
-		            <div class="row">
-						<div class="col-md-3">
-							
-								<img style="float: left; margin-right: 20px; width: 100%;" width="" class="thumbnail" src="<?php echo base_url() . 'assets/img/books_image/' . '{image}' ?>">
-								<!-- Your rating: 
-								<span class="ratings">
-									<span data="1" title="Rate 1 out of 5" class="glyphicon glyphicon-star-empty"> </span>
-									<span data="2" title="Rate 2 out of 5" class="glyphicon glyphicon-star-empty"> </span>
-									<span data="3" title="Rate 3 out of 5" class="glyphicon glyphicon-star-empty"> </span>
-									<span data="4" title="Rate 4 out of 5" class="glyphicon glyphicon-star-empty"> </span>
-									<span data="5" title="Rate 5 out of 5" class="glyphicon glyphicon-star-empty"> </span>
-									
-								</span>
+            <div style="padding-left: 20px;" class="panel-footer">
 
-								
-									<h4>Overall Ratings: <strong><span id="rates">4.5</span></strong></h4>
-									<h5>Out of 10,000 people rated.</h5> -->
-								
-		
- 
-						</div>
-						<div class="col-md-3">
-							<h6 id="title" data="{product_id}"><strong>{title}</strong></h6>
-							<small> 
-							   Author: {author} <br/>
-							   Publisher: {publisher} <br/>
-							   Format: {format} <br/>
-							   ISBN: {isbn} <br/>
-							   </small>
-							 
-							   <span class="label label-success">PHP {price}</span><small><br/>
-							  	This product will ship in 2 to 3 days.<br/>
-							   </small><br/>
-						</div>
-						<div class="col-md-6">
-							<h4>Description</h4>
-							<p>{description}</p>
-						</div>
-					</div>	
+            <div  class="row text-center" >
+            {search_result}
+            <div style="margin-bottom: 10px;" class="col-md-3">
+            <a href="<?php echo base_url() . 'book/view/' . '{product_id}/{product_url}' ?>" title="{0} by {1}">
+            <img class="image-size thumbnail" src="<?php echo base_url() . 'assets/img/books_image/{image}' ?>">	
+            <small><strong><span class="text-center">{title}</span></strong> <br/> by {author}</small>
+            </a>
+            <hr>
+            <div><button class="btn btn-xs btn-danger">Price: PHP {price}</button> <br> 
+	            {available}
+	            <a style="margin-bottom: 2px;" href="<?php echo site_url('book/add_wishlist/{product_id}') ?>" class="label label-info">Add  Wishlist</a>
+            </div>
+            </div>
+            {/search_result}
 
-					 <div class="text-center col-md-12 alert alert-primary">
-							   <hr/>
-                					
-							   			<h4 class="">PHP {price}</h4>
-							   				
-							   				
-
-							   				Qty: <input class="" value="1" name="qty" id="qty" type="text" size=6> <a id="add_cart" href="<?php echo site_url('cart/add/{product_id}/1/')?>" class="btn btn-info">Add to cart</a><a class="btn btn-danger" href="<?php echo site_url('book/add_wishlist/{product_id}') ?>">Add to Wishlist</a>
-							   		
-							   			<script type="text/javascript">
-							   				$(document).on('keyup', '#qty', function(){
-							   					var value = $('#qty').val();
-							   					var link = "<?php echo site_url();?>";
-							   					link = link.concat('cart/add/{product_id}/');
-							   					link = link.concat(value);
-							   					var pathname = window.location.pathname;
-							   					link = link.concat(pathname);
-
-							   					$("#add_cart").attr("href", link);
-
-							   				});
-							   				</script>
-							   		
-
-              					
-					 </div>
 
             </div>
-            
-            <div class="panel-footer">
+            <hr/>
+             
+               <center>
+         {pagination}
+
+            </center>
             </div>
+          </div>
+</div>
 </div>
 
-{/search_result}
-
-
-		
-	</div>
 	<div class="col-md-3">
 		<div class="panel panel-primary" id="panels">
             <div class="panel-heading">My Cart</div>
@@ -125,77 +268,98 @@
 		</div>
 	</div>
 
-
-
 </div>
 
-<script type="text/javascript">
-    
 
-$(function(){
+   
+            <script type="text/javascript">
+         
+            $(function(){
+
+            	$(document).on('change', '.order_by', function(){
+
+            		var cat = $('.category').val();
+            		var order_by = $('.order_by').val();
+            		var limit = $('.limit').val();
+            		var base = '<?php echo site_url().'book/browse/'?>';
+            		var mode = '<?php echo $link_segments['mode']?>';
+            		var page = '<?php echo $link_segments['page']?>';
+            		var order = '<?php echo $link_segments['order']?>';
+            		
+            			if (mode == '') {
+            				mode = 'grid';
+            			}
+            			if(order == ''){
+            				order = 'asc'; 
+            			}
+            			if (limit == '') {
+            				limit == 10;
+            			}
+            			if (order_by == '') {
+            				order_by = 'name';
+            			}
+            			if (page == '') {
+            				page = 0;
+            			}
+            			
+            			
+            		var link = base+''+cat+'/'+mode+'/'+order_by+'/'+order+'/'+limit+'/'+page;
+            
+            		window.location.replace(link);
+
+            	})
+
+            	$(document).on('change', '.limit', function(){
+
+            		var cat = $('.category').val();
+            		var limit = $('.limit').val();
+            		var base = '<?php echo site_url().'book/browse/'?>'
+            		
+					var mode = '<?php echo $link_segments['mode']?>'
+            		var order = '<?php echo $link_segments['order']?>'
+            		var page = '<?php echo $link_segments['page']?>'
+            		var order_by = '<?php echo $link_segments['order_by']?>';
+            		
+
+            			if (mode == '') {
+            				mode = 'grid';
+            			}
+            			if(order == ''){
+            				order = 'asc'; 
+            			}
+            			if (order_by == '') {
+            				order_by = 'name';
+            			}
+            			if (limit =='') {
+            				limit == 10;
+            			}
+            			
+            			
+            		var link = base+''+cat+'/'+mode+'/'+order_by+'/'+order+'/'+limit+'/'+page;
+            		window.location.replace(link);
+
+            	})
 
 
+            	$(document).on('change', '.category', function(){
 
-	$(document).on('click', '.glyphicon', function(event) {
-        event.preventDefault();
-        /* Act on the event */
+            		var cat = $('.category').val();
+            		var base = '<?php echo site_url().'book/browse/'?>'
+            		// var mode = '<?php echo $link_segments['mode']?>'
+            		// var order = '<?php echo $link_segments['order']?>'
+            		// var limit = '<?php echo $link_segments['limit']?>'
+            		// var page = '<?php echo $link_segments['page']?>'
 
-        var data = $(this).attr('data');
-       	var product_id = $('#title').attr('data');
+            		if (cat == '') {
+            			cat = 'all';
+            		}	
 
+            		var link = base+''+cat;
+            		window.location.replace(link);
 
-       $.ajax({
-       	url: '<?php echo site_url("book/ratings"); ?>',
-       	type: 'POST',
-       	data: {rate: data, product_id: product_id, user_id: 40},
-       })
-       .done(function() {
-       	console.log("success");
-
-       	// location.reload();
-
-       })
-       .fail(function() {
-       	console.log("error");
-       })
-       .always(function() {
-       	console.log("complete");
-       });
-       
+            	})
 
 
-    });
+            });
 
-
-
-    $(document).on('mouseenter', '.glyphicon', function(event) {
-        event.preventDefault();
-        /* Act on the event */
-
-        
-        $(this).addClass('glyphicon-star');
-		$(this).removeClass('glyphicon-star-empty');       
-        //alert('sdaasd');
-
-    });
-
-
-    $(document).on('mouseout', '.glyphicon', function(event) {
-        event.preventDefault();
-        /* Act on the event */
-
-        
-      	$('.ratings span').each(function(index, val) {
-      		 /* iterate through array or object */
-      		 $(this).removeClass('glyphicon-star');
-      		$(this).addClass('glyphicon-star-empty');  
-
-      	});
-
-    });
-
-
-
-});
-
-</script>
+            </script>
