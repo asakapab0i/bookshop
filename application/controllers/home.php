8f070f48ebd2b357791db82e53902d40a57ae5fc
Bryan Bojorque <bojorquebryan@gmail.com>
2014-02-09 22:08:11 +0000

    added ratings panel and removed random buggy function

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -18,8 +18,10 @@ class Home extends CI_Controller {
 		//Prepare Main Data
 		$main['featured'] = $featured = $this->home_model->home_featured();
 		$main['releases'] = $releases = $this->home_model->home_releases();
-		$main['random'] = $random = $this->home_model->home_random();
-		//var_dump($main);
+		$main['ratings'] = $ratings = $this->home_model->home_ratings();
+                $main['random'] = $random = $this->home_model->home_random();
+
+                //var_dump($main);
 
 
 		foreach ($featured as $key => $value) {
@@ -38,15 +40,21 @@ class Home extends CI_Controller {
 
 		}
 
-		foreach ($random as $key => $value) {
+		foreach ($ratings as $key => $value) {
 				
 				foreach ($value as $key2 => $value2) {
-					$main['random'][$key]['title'] = character_limiter($random[$key]['title'], 20);
+					$main['ratings'][$key]['title'] = character_limiter($ratings[$key]['title'], 20);
 				}
 
 		}
 
+        	foreach ($random as $key => $value) {
+				
+				foreach ($value as $key2 => $value2) {
+					$main['random'][$key]['title'] = character_limiter($ratings[$key]['title'], 20);
+				}
 
+		}
 
 
 		//Page Header
